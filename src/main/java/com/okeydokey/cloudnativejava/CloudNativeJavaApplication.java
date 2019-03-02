19eaef0a14936d033445845433a338d19d8323dc
okeydokey <yunkim28@gmail.com>
2019-03-02 17:24:02 +0900

    first commit

---
+++ src/main/java/com/okeydokey/cloudnativejava/CloudNativeJavaApplication.java
@@ -0,0 +1,13 @@
+package com.okeydokey.cloudnativejava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudNativeJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudNativeJavaApplication.class, args);
+    }
+
+}
